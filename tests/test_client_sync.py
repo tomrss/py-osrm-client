0a85c8f59410a48d469bbfbaf69f5acf360e9208
tomrss <tommaso.rossi1@protonmail.com>
2023-12-21 19:57:53 +0100

    test: Add test cases and aiohttp async mock

--- tests/test_client_sync.py
+++ tests/test_client_sync.py
@@ -19,3 +19,30 @@ def test_route(froute, requests_mock):
         route = osrm.route(froute["coords"], steps=True)
 
     froute["assertions"](route)
+
+
+def test_table(ftable, requests_mock):
+    requests_mock.get(ftable["url"], json=json.loads(ftable["res_json"]))
+
+    with OsrmClient() as osrm:
+        table = osrm.table(ftable["coords"])
+
+    ftable["assertions"](table)
+
+
+def test_match(fmatch, requests_mock):
+    requests_mock.get(fmatch["url"], json=json.loads(fmatch["res_json"]))
+
+    with OsrmClient() as osrm:
+        match = osrm.match(fmatch["coords"], steps=True)
+
+    fmatch["assertions"](match)
+
+
+def test_trip(ftrip, requests_mock):
+    requests_mock.get(ftrip["url"], json=json.loads(ftrip["res_json"]))
+
+    with OsrmClient() as osrm:
+        trip = osrm.trip(ftrip["coords"], steps=True)
+
+    ftrip["assertions"](trip)
